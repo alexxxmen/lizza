ada064d965da37ca1edb8db2ef2f7fe7fa01650a
alexxxmen <alexxxmen1990@gmail.com>
2016-04-07 21:42:43 +0300

    - Add model Category

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,16 @@
-from __future__ import unicode_literals
+# -*- coding:utf-8 -*-
+# from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.CharField(max_length=300)
+
+    # возможные поля:
+    # - картинка
+
+    def __str__(self):
+        return self.title
+
