7056f693d6151979b6336e62fd085a5306203606
alexxxmen <alexxxmen1990@gmail.com>
2016-04-07 22:39:23 +0300

    - Add model Product  - Migrate

--- todo/models.py
+++ todo/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100)
-    desc = models.CharField(max_length=300)
+    title = models.CharField(max_length=100, verbose_name='Название категории')
+    desc = models.CharField(max_length=300, verbose_name='Описание категории')
 
     # возможные поля:
     # - картинка
@@ -16,10 +16,21 @@ class Category(models.Model):
 
 
 class Feedback(models.Model):
-    name = models.CharField(max_length=100)
-    subject = models.CharField(max_length=150)
-    email = models.EmailField()
-    text = models.TextField(max_length=1000)
+    name = models.CharField(max_length=100, verbose_name='Имя пользователя')
+    subject = models.CharField(max_length=150, verbose_name='Тема отзыва')
+    email = models.EmailField(verbose_name='Почта пользователя')
+    text = models.TextField(max_length=1000, verbose_name='Текст отзыва')
 
     def __str__(self):
         return self.subject[:50]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название товара')
+    desc = models.CharField(max_length=500, blank=True, verbose_name='Описание товара')
+    count = models.IntegerField(default=0, verbose_name='Количество товара')
+    category = models.ForeignKey(Category, verbose_name='Категория товара')
+    img = models.ImageField(upload_to='images', verbose_name='Картинка товара')
+
+    def __str__(self):
+        return self.name
