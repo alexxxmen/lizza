bdfcc84f92bf1df632de32c919b201b3beee9f12
alexxxmen <alexxxmen1990@gmail.com>
2016-04-12 21:43:41 +0300

    - Add Admin settings for Product  - Add text fields to models desc

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,54 @@
+# -*- coding:utf-8 -*-
 from django.contrib import admin
 from todo.models import Order, Category, Product, Feedback
 
+
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'product_code', 'count',
+                    'create_date', 'category', 'modified', 'status')
+    list_display_links = ('name',)
+    search_fields = ['name', 'product_code', 'create_date',
+                     'modified', 'status', 'category']
+    list_filter = ['create_date', 'category', 'modified',
+                   'status', 'count']
+    readonly_fields = ('create_date', 'modified')
+    fieldsets = [
+        ('Главное', {'fields': [('name', 'slug'), ('product_code','img')]}),
+        ('Детально', {'fields': [('category', 'status'), ('count',)]}),
+        ('Дополнительно', {'fields': [('short_desc', 'full_desc'), ('create_date', 'modified')]})
+    ]
+
+    def make_instock(self, request, queryset):
+        updated = queryset.update(status='S')
+        if updated == 1:
+            message_b = '1-й записи был'
+        else:
+            message_b = '%s записей был' %updated
+        self.message_user(request, 'Статус %s успешно изменен на \'Есть в наличии\'' % message_b)
+    make_instock.short_description = 'Товар есть в наличии'
+
+    def make_not_available(self, request, queryset):
+        updated = queryset.update(status='N')
+        if updated == 1:
+            message_b = '1-й записи был'
+        else:
+            message_b = '%s записей был' %updated
+        self.message_user(request, 'Статус %s успешно изменен на \'Нет в продаже\'' % message_b)
+    make_not_available.short_description = 'Товара нет в продаже'
+
+    def make_on_order(self, request, queryset):
+        updated = queryset.update(status='O')
+        if updated == 1:
+            message_b = '1-й записи был'
+        else:
+            message_b = '%s записей был' %updated
+        self.message_user(request, 'Статус %s успешно изменен на \'Под заказ\'' % message_b)
+    make_on_order.short_description = 'Товар под заказ'
+
+    actions = ['make_instock', 'make_not_available', 'make_on_order']
+
 admin.site.register(Order)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
--- todo/models.py
+++ todo/models.py
@@ -7,7 +7,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Category(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название категории')
-    desc = models.CharField(max_length=300, blank=True, null=True, verbose_name='Описание категории')
+    desc = models.TextField(max_length=300, blank=True, null=True, verbose_name='Описание категории')
 
     # возможные поля:
     # - картинка
@@ -36,18 +36,19 @@ class Feedback(models.Model):
 
 class Product(models.Model):
     PRODUCT_STATUS = (
-        ('Y', 'В наличии'),
-        ('N', 'Нет в наличии'),
+        ('S', 'В наличии'),
+        ('N', 'Нет в продаже'),
         ('O', 'Под заказ'),
     )
     product_code = models.CharField(max_length=30, null=True, unique=True, verbose_name='product_id')
     name = models.CharField(max_length=100, verbose_name='Название товара')
-    desc = models.CharField(max_length=500, blank=True, verbose_name='Описание товара')
-    pub_date = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    short_desc = models.TextField(max_length=300, blank=True, verbose_name='Краткое описание товара')
+    full_desc = models.TextField(blank=True, verbose_name='Полное описание товара')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    modified = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     count = models.IntegerField(default=0, verbose_name='Количество товара')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='category')
-    img = models.ImageField(upload_to='images', blank=True, verbose_name='Картинка товара')
+    img = models.ImageField(upload_to='media/images/product', blank=True, verbose_name='Картинка товара')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='slug')
     status = models.CharField(max_length=1, choices=PRODUCT_STATUS, default='O', verbose_name='Статус товара')
 
