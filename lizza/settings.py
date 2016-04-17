5bb3a3460298085167ea8df68b1400b2823c7a2f
alexxxmen <alexxxmen1990@gmail.com>
2016-04-18 00:33:44 +0300

    - add footer.html  - add jquery.js  - rename base to index

--- lizza/settings.py
+++ lizza/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lizza.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'test_templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
